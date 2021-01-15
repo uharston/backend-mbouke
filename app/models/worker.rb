019721ce30398d909d830ca3852ebd9e00ecaa01
uharston <uharston@gmail.com>
2021-01-15 16:58:25 -0600

    Add: Worker model validations

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,2 +1,13 @@
 class Worker < ApplicationRecord
+    # validates :email, uniqueness: true
+    validates :service, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :country, presence: true
+    validates :city, presence: true
+    validates :business_name, presence: true
+    validates :phone_number, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, presence: true
 end
